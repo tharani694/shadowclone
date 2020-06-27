218a9d327cbec34f62368808c300d2f5fe4da0e4
Tharani718 <tharani12ia@gmail.com>
2020-06-27 21:37:37 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# what's it about?
+
+An easier way to get perfect shadows for a non-designer.
+
+# How to install?
+
+`npm i shadowclone --save`
+
+# how to use it?
+
+'''
+import { shadowclone } from 'shadowclone';
+
+shadowclone ({
+    shadow_type: 'soft',
+    padding:false
+});
+
+'''
+
+# Options
+
+Shadowclone supports 2 great options, both of which are optional:
+ 
+ * *shadow_type* - hard | soft --> (Defaults to soft)
+ * *padding* - boolean --> (Defaults to false)
---
+++ index.js
@@ -0,0 +1,18 @@
+function shadowclone(options) {
+    let images = document.querySelectorAll('.shadowclone');
+
+    if(options.shadow_type === 'hard')
+        options.shawdow_type = '0px'
+    else
+        options.shadow_type = '15px'
+
+    images.forEach(image =>{
+        image.style.boxShadow = `10px 10px ${options.shadow_type} ipx rgba(0,0,0,0.12)`;
+    
+        if(options.padding){
+            image.style.padding = `1em`;
+        }
+    })
+}
+
+module.exports.shadowclone = shadowclone;
